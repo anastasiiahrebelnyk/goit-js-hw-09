55bc0803de0a46e089f59e69994925113dcf2451
anastasiiahrebelnyk <anastasiiahrebelnyk@gmail.com>
2026-02-26 14:05:50 +0200

    form markup and js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,5 +1,59 @@
-console.log("form");
-function foo() {
-    console.log('This is a function!');
+const STORAGE_KEY = "feedback-form-state";
+
+const formEl = document.querySelector('form');
+
+formEl.addEventListener('input', (e) => {
+    const formData = new FormData(formEl);
+    const obj = {
+        email: formData.get('email'),
+        message: formData.get('message'),
+    }
+    saveToolS(STORAGE_KEY, obj);
+});
+
+document.addEventListener('DOMContentLoaded', e => {
+    const userForm = loadFromLS(STORAGE_KEY, {});
+    formEl.elements.email.value = userForm.email || '';
+    formEl.elements.message.value = userForm.message || '';
+});
+
+formEl.addEventListener('submit', e => {
+    e.preventDefault();
+    if (formEl.elements.email.value === "" || formEl.elements.message.value === "") {
+        return alert('Fill please all fields');
+
+    };
+
+    const formData = new FormData(formEl);
+    const obj = {
+        email: formData.get('email'),
+        message: formData.get('message'),
+    }
+    console.log(obj);
+    
+
+
+
+    localStorage.removeItem(STORAGE_KEY);
+    formEl.reset();
+});
+
+
+
+
+
+
+function saveToolS(key, value) {
+  const json = JSON.stringify(value);
+  localStorage.setItem(key, json);
 }
-console.log(JSON.stringify(foo));
+
+function loadFromLS(key, defaultValue) {
+  const jsonData = localStorage.getItem(key);
+  try {
+    const data = JSON.parse(jsonData);
+    return data ?? defaultValue;
+  } catch {
+    return jsonData ?? defaultValue;
+  }
+}
