8028b2d4faa283462cf160095e35165d9b44188d
anastasiiahrebelnyk <anastasiiahrebelnyk@gmail.com>
2026-02-26 10:03:16 +0000

    Deploying to gh-pages from @ anastasiiahrebelnyk/goit-js-hw-09@d8f638ca0d2a8e2387c997f2a9bceaea25d1fff1 🚀

--- 2-form.js
+++ 2-form.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */console.log("form");
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */console.log("form");function o(){console.log("This is a function!")}console.log(JSON.stringify(o));
 //# sourceMappingURL=2-form.js.map
